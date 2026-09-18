49825d712cc12cc79dde2a1c007b63005783e3fe
maxleekorea <max.lee.korea@gmail.com>
2026-09-18 12:10:48 +0900

    Adapt community Pages function for Workers runtime

---
+++ community/src/index.js
@@ -0,0 +1,14 @@
+import { onRequest } from "../functions/[[path]].js";
+
+export default {
+  async fetch(request, env, ctx) {
+    return onRequest({
+      request,
+      env,
+      waitUntil: ctx.waitUntil.bind(ctx),
+      passThroughOnException: ctx.passThroughOnException?.bind(ctx),
+      params: {},
+      data: {}
+    });
+  }
+};
