5f2452a2eb1523c0c5f87c63bcf1432d25b3f4e5
maxleekorea <max.lee.korea@gmail.com>
2026-09-24 10:27:36 +0900

    Add course sales page content fields

---
+++ worker/migrations/0017_course_sales_page_content.sql
@@ -0,0 +1,6 @@
+ALTER TABLE courses ADD COLUMN instructor_name TEXT;
+ALTER TABLE courses ADD COLUMN instructor_bio TEXT;
+ALTER TABLE courses ADD COLUMN target_audience TEXT;
+ALTER TABLE courses ADD COLUMN learning_outcomes TEXT;
+ALTER TABLE courses ADD COLUMN access_info TEXT;
+ALTER TABLE courses ADD COLUMN refund_policy_text TEXT;
