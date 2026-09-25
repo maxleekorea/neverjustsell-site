510bdea6e03835c445e1c19a62bef1eb87375980
maxleekorea <max.lee.korea@gmail.com>
2026-09-25 22:53:28 +0900

    Bump refund E2E production build

--- worker/src/production.js
+++ worker/src/production.js
@@ -10,7 +10,7 @@ import programHostApp from "./program-host.js";
 import { ensureProgramSchema } from "./program-schema.js";
 import { runPendingSystemOperations, getPaymentE2EProductStatus, getPaymentE2EFlowStatus, getCafe24CatalogStatus, getAllCurrentProductsShippingStatus, getDigitalProductPropertyVisibilityStatus, getDigitalProductDetailUxStatus } from "./system-operations.js";
 
-const PRODUCTION_BUILD = "2026-09-25-refund-e2e-v5";
+const PRODUCTION_BUILD = "2026-09-25-refund-e2e-v6";
 import {
   CLASSROOM_ORIGIN,
   LEGACY_CLASSROOM_HOST,
